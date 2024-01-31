71623170cc5fc013fa0a95023c0cfd68e2ef4dd9
unknown <139689090+resforx@users.noreply.github.com>
2024-02-01 00:17:21 +0900

    update 9 files

---
+++ src/main/pm5.d.ts
@@ -0,0 +1,8 @@
+interface str {
+    /**
+     * string to int value to its value
+     * @param value A value of its string to parse int
+     */
+    parseInt(value: string): number,
+    indexOf(scrString: string): number
+}
