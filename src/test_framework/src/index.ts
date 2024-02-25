4ab876c94a05cff322ce89c89efb0aa4a0284feb
unknown <139689090+resforx@users.noreply.github.com>
2024-02-26 00:26:43 +0900

    Update index.ts

--- src/test_framework/src/index.ts
+++ src/test_framework/src/index.ts
@@ -149,15 +149,15 @@ const Didact = {
   render
 };
 
-/** @jsx Didact.createElement */
-const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align:right">from Didact</h2>
-  </div>
-);
+// /** @jsx Didact.createElement */
+// const element = (
+//   <div style="background: salmon">
+//     <h1>Hello World</h1>
+//     <h2 style="text-align:right">from Didact</h2>
+//   </div>
+// );
 
-const container = document.getElementById("root");
-if (container) {
-  Didact.render(element, container);
-}
+// const container = document.getElementById("root");
+// if (container) {
+//   Didact.render(element, container);
+// }
