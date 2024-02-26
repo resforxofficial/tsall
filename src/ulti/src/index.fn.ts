17ccc35724d4e87ce6d99e327d21e62fcabca1cd
unknown <139689090+resforx@users.noreply.github.com>
2024-02-26 15:41:59 +0900

    Create index.fn.ts

---
+++ src/ulti/src/index.fn.ts
@@ -0,0 +1,23 @@
+/**
+ * test or study of typescript hd function
+ */
+
+export {};
+
+function testset<T>(func: (value: T) => void, arr: T[]): void {
+    arr.forEach(func);
+}
+
+const a = testset<string>((value) => {
+    console.log(value);
+}, ["hello", "world"]);
+
+function testset1<T>(func: (value: T) => void, num: T): void {
+    func(num);
+}
+
+const a1 = testset1<number>((value) => {
+    for (let i = 0; i < value; i++) {
+        console.log(i);
+    }
+}, 5);
